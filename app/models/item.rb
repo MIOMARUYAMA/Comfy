f4580207c52c89ebf3af2177919199489ac794c1
MIOMARUYAMA <106859222+MIOMARUYAMA@users.noreply.github.com>
2022-12-10 05:12:36 +0000

    image

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,10 @@ class Item < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   def get_image(width, height)
-    # unless image.attached?
-    #   file_path = Rails.root.join('app/assets/images/icon_002680_256.png')
-    #   image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/pmg')
-    # end
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/icon_002680_256.png')
+      image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/pmg')
+    end
     image.variant(resize_to_limit: [width, height]).processed
   end
 end
